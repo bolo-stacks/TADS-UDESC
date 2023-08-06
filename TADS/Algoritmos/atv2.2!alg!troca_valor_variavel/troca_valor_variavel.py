aca5d8a578dd99bdf7cb89e1e3a8ed87f754a7f9
pablostacks <gonzalezbryahoo@gmail.com>
2023-08-06 20:02:33 -0300

    V.2

--- TADS/Algoritmos/atv2.2!alg!troca_valor_variavel/troca_valor_variavel.py
+++ TADS/Algoritmos/atv2.2!alg!troca_valor_variavel/troca_valor_variavel.py
@@ -3,9 +3,11 @@ Esse programa permite trocar o valor das
 variáveis 
 """
 
-print('Programa que inverte o valor das variáveis A e B')               # imprime na tela
-a = int(input("Entre com o valor de A: "))                              # entrada de dados para a variável A
-b = int(input("Entre com o valor de B: "))                              # entrada de dados para a variável B
-print("O valor de A é ", a, " e o valor de B é ", b)                    # mostra na tela os valores definidos pelo
-(a, b) = (b, a)                                                         # fórmula para troca dos valores das variáveis
-print("O novo valor de A é ", a, " e o novo valor de B é ", b)          # mostra na tela o novo valor trocado
+print('Programa que inverte o valor das variáveis A e B')  # imprime na tela
+a = int(input("Entre com o valor de A: "))  # entrada de dados para a variável A
+b = int(input("Entre com o valor de B: "))  # entrada de dados para a variável B
+print("O valor de A é ", a, " e o valor de B é ", b)  # mostra na tela os valores definidos pelo
+(a, b) = (b, a)  # fórmula para troca dos valores das variáveis
+print("O novo valor de A é ", a, " e o novo valor de B é ", b)  # mostra na tela o novo valor trocado
+
+# alterando programa teste

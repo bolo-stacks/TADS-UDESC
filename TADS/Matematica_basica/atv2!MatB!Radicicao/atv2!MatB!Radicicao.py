cb6be55c73a2aabc108875775f513d0b3c06639f
pablostacks <gonzalezbryahoo@gmail.com>
2023-08-18 20:41:11 -0300

    V.2.1

---
+++ TADS/Matematica_basica/atv2!MatB!Radicicao/atv2!MatB!Radicicao.py
@@ -0,0 +1,21 @@
+"""
+Testar a programação das linhas a seguir no
+seu editor Python. Fazer um programa para resolver
+a radiciação do número 9. Este exercício não é
+necessário entregar, basta treinar no Python.
+"""
+
+# Exemplo de potenciação
+
+print("base ^ expoente:")
+base = int(input("Base: "))
+expoente = int(input("Expoente: "))
+
+potencia = 1
+count = 1
+
+while count <= expoente:
+    potencia *= base
+    count += 1
+
+print(base, "^", expoente, "=", potencia)

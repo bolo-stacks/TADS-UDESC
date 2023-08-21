3839ac44aa693ae8524b99d5142fb727e5b2e719
pablostacks <gonzalezbryahoo@gmail.com>
2023-08-20 22:00:34 -0300

    V.2.1

--- TADS/Exercicios_PDF_Treino/atv4.6!Python!Taxi_If_Else/atv4.6!Python!Taxi_If_Else.py
+++ TADS/Exercicios_PDF_Treino/atv4.6!Python!Taxi_If_Else/atv4.6!Python!Taxi_If_Else.py
@@ -6,16 +6,25 @@ cobrando R$ 0,50 por km para viagens de até de 200 km,
 e R$ 0,45 para viagens mais longas.
 """
 
-print('Calculo do valor da passagem de\nacordo com a distância.')
-dist = float(input('Entre com a distância a ser percorrida em quilômetros:'))
 
-if dist <= 200:
-    tot = dist * 0.50
-    print('O valor da passagem para %.0f quilômetros é R$%.2f.' % (dist, tot))
+def retorna():
+    print('Calculo do valor da passagem de\nacordo com a distância.')
+    dist = float(input('Entre com a distância a ser percorrida em quilômetros:'))
 
-if dist > 200:
-    tot = dist * 0.45
-    print('O valor da passagem para %.0f quilômetros é R$%.2f.' % (dist, tot))
+    if dist <= 200:
+        tot = dist * 0.50
+        print('O valor da passagem para %.0f quilômetros é R$%.2f.' % (dist, tot))
+
+    if dist > 200:
+        tot = dist * 0.45
+        print('O valor da passagem para %.0f quilômetros é R$%.2f.' % (dist, tot))
 
 
+exec1 = True
 
+while exec1:
+    retorna()
+    op = str(input('Quer continuar a ler os números por extenso? [S/N] '))
+    if op == 'n' or op == 'N':
+        exec1 = False
+        exit('Obrigado por usar nosso programa.')
--- TADS/Exercicios_PDF_Treino/atvLoop!Python!TesteLoop/atvLoop!Python!TesteLoop.py
+++ TADS/Exercicios_PDF_Treino/atvLoop!Python!TesteLoop/atvLoop!Python!TesteLoop.py
@@ -1,19 +1,21 @@
-num_por_extenso = [
-    'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 
-    'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte']
+"""
+Aprendendo loops
+"""
 
 
-def retorna_numero_por_extenso():
+# loop 1
+
+def retorna():
     numero = int(input('Digite um número: '))
-    numero_extenso = num_por_extenso[numero]
-    print(f'Você digitou o número {numero_extenso}')
+    print(f'Você digitou o número {numero}')
 
 
 exec1 = True
 
 while exec1:
-    retorna_numero_por_extenso()
-    op = str(input('Quer continuar a ler os números por extenso? [S/N] ')).upper().strip()
-    if op == 'N':
+    retorna()
+    op = str(input('Quer continuar a ler os números por extenso? [S/N] '))
+    if op == 'n':
         exec1 = False
         exit('Obrigado por usar nosso programa.')
+

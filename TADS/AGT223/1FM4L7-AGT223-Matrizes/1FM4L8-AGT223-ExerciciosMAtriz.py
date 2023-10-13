0ac69ed0fc58eb87621544a844a0878107ba5173
bolo-stacks <bolo_stacks@outlook.com>
2023-10-13 16:12:57 -0300

    commit v1.0

--- TADS/AGT223/1FM4L7-AGT223-Matrizes/1FM4L8-AGT223-ExerciciosMAtriz.py
+++ TADS/AGT223/1FM4L7-AGT223-Matrizes/1FM4L8-AGT223-ExerciciosMAtriz.py
@@ -9,6 +9,9 @@ Exercícios com Matriz valendo nota
 
 
 # Exercício 2
+'''
+Faça um programa em Python para preencher uma matriz M de dimensão 5x5 com uma sequência numérica iniciando em 0 e adicionando valores coluna a coluna iniciando na primeira linha e terminando na quinta, conforme exemplo abaixo:
+'''
 
 # Inicializa a matriz 5x5
 # matriz = [[0 for _ in range(5)] for _ in range(5)]
@@ -25,9 +28,16 @@ Exercícios com Matriz valendo nota
 # Imprime a matriz no formato desejado
 # print(matriz)
 
+# Versão do professor do exercício 2
+# matriz = [[j+(5*i) for j in range(5)] for i in range(5)]
+# print(matriz)
+
 
 # Exercício 3
 
+'''
+Faça um programa Python para somar todos os elementos da matriz do exercício anterior e calcular a média dos valores. Exiba ao final uma linha com a soma dos valores (valor inteiro) e outra linha com a média dos valores da matriz com uma casa decimal.
+'''
 
 # Inicializa a matriz 5x5
 # matriz = [[0 for _ in range(5)] for _ in range(5)]
@@ -52,8 +62,36 @@ Exercícios com Matriz valendo nota
 # print(soma)
 # print(media)
 
+# Versão 1 do professor do exercício 3
+
+# matriz = [[j+(i*5) for j in range(5)] for i in range(5)]
+# soma = 0
+# for i in range(5):
+#    soma = soma + sum(matriz[i])
+# print(soma)
+# print("%.1f" % (soma/25.0))
+
+# Versão 2 do professor do exercício 3
+
+# matriz = [[j+(i*5) for j in range(5)] for i in range(5)]
+# soma = 0
+# for i in range(5):
+#    for j in range(5):
+#       soma = soma + matriz[i][j]
+# print(soma)
+# media = soma/25.0
+# print("%.1f" % (media))
 
 # Exercicio 4
+'''
+Faça um programa para:
+
+    Criar uma matriz 5x4 preenchendo em cada célula a multiplicação dos índices da matriz. Por exemplo, na posição [2][3] será colocado o valor 6 (2x3);
+    O programa deve ler 12 valores inteiros, representando 6 posições (e 3 pares de posições). A cada quatro valores, representando duas posições na matriz, faça a troca dos valores na matriz;
+    Faça as três trocas (para os 3 pares de posições);
+    Mostre a matriz ao final usando print(matriz).
+
+'''
 
 # matriz = [[i * j for j in range(4)] for i in range(5)]
 # valores = []
@@ -68,8 +106,30 @@ Exercícios com Matriz valendo nota
 
 # print(matriz)
 
+# Versão do professor do exercício 4
+
+# matriz = [[i*j for j in range(4)] for i in range(5)]
+# for i in range(3):
+#     l1 = int(input())
+#     c1 = int(input())
+#     l2 = int(input())
+#     c2 = int(input())
+#     aux = matriz[l1][c1]
+#     matriz[l1][c1] = matriz[l2][c2]
+#     matriz[l2][c2] = aux
+# print(matriz)
 
 # Exercício 5
+
+'''
+Faça um programa para:
+
+    Criar uma matriz 5x4 preenchendo em cada célula a multiplicação dos índices da matriz. Por exemplo, na posição [2][3] será colocado o valor 6 (2x3);
+    O programa deve ler 12 valores inteiros, representando 6 posições (e 3 pares de posições). A cada quatro valores, representando duas posições na matriz, faça a troca dos valores na matriz;
+    Faça as três trocas (para os 3 pares de posições);
+    Mostre a matriz ao final usando print(matriz).
+
+'''
 
 # L = int(input())
 # C = int(input())
@@ -83,4 +143,17 @@ Exercícios com Matriz valendo nota
 #         linha.append(valor)
 #     matriz.append(linha)
 
+# print(matriz)
+
+# Versão do professor do exercício 5
+
+# matriz = [[i*j for j in range(4)] for i in range(5)]
+# for i in range(3):
+#     l1 = int(input())
+#     c1 = int(input())
+#     l2 = int(input())
+#     c2 = int(input())
+#     aux = matriz[l1][c1]
+#     matriz[l1][c1] = matriz[l2][c2]
+#     matriz[l2][c2] = aux
 # print(matriz)
